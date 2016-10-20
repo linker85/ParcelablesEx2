42f9e6e0306459b947db2d08e43106180124bfab
linker85 <linker85@hotmail.com>
2016-10-20 18:08:22 -0400

    parcelables and 2 way intents

--- app/src/main/java/com/example/com/parcelablesex2/Main2Activity.java
+++ app/src/main/java/com/example/com/parcelablesex2/Main2Activity.java
@@ -1,8 +1,10 @@
 package com.example.com.parcelablesex2;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 
 import java.util.List;
 
@@ -10,6 +12,7 @@ public class Main2Activity extends AppCompatActivity {
 
     private static final String MY_KEY = "parcelable";
     private static final String TAG = "Main2ActivityTAG_";
+    public static final String MY_KEY_RESULT = "MY_RESULT_KEY";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,5 +24,12 @@ public class Main2Activity extends AppCompatActivity {
         } else {
             Log.d(TAG, "onCreate: No users");
         }
+    }
+
+    public void startActivityForResultBack(View view) {
+        Intent intent = new Intent();
+        intent.putExtra(MY_KEY_RESULT, "RESULT_FROM_SECOND");
+        setResult(MainActivity.REQUEST_KEY, intent); // Send the intent back to the main activity
+        finish();
     }
 }
--- app/src/main/java/com/example/com/parcelablesex2/MainActivity.java
+++ app/src/main/java/com/example/com/parcelablesex2/MainActivity.java
@@ -14,6 +14,7 @@ public class MainActivity extends AppCompatActivity {
 
     private static final String MY_KEY = "parcelable";
     private static final String TAG = "MainActivityTAG_";
+    public static final int REQUEST_KEY = 10;
     private ArrayList<User> users;
 
     @Override
@@ -43,5 +44,41 @@ public class MainActivity extends AppCompatActivity {
         Intent intent = new Intent(this, Main2Activity.class);
         intent.putExtra(MY_KEY, users);
         startActivity(intent);
+    }
+
+    // Tells something to android system from our app. Ex pass coordinates from our app to a Google map
+    // 1. set and action (Android does not know who is going to manage)
+    public void implicitIntent(View view) {
+        // Create the text message with a string
+        Intent sendIntent = new Intent();
+        sendIntent.setAction(Intent.ACTION_SEND);// if we add a filter in the manifest we can add them in the setAction to open filtered 3erd party tools
+        sendIntent.putExtra(Intent.EXTRA_TEXT, "Edwin is the best");
+        sendIntent.setType("text/plain");
+
+        // Verify that the intent will resolve to an activity
+        // If email -> the message goes to the body
+        if (sendIntent.resolveActivity(getPackageManager()) != null) {
+            startActivity(sendIntent);
+        }
+    }
+
+    // Knows what exact class it has to show, you pass fully qualified name
+    public void explicitIntent(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivity(intent);
+    }
+
+    // Creates a 2 way binding (Can send data without creating a new intent)
+    public void startActivityForResult(View view) {
+        Intent intent = new Intent(this, Main2Activity.class);
+        startActivityForResult(intent, REQUEST_KEY);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.d(TAG, "requestCode: " + requestCode);
+        Log.d(TAG, "onActivityResult: " + resultCode);
+        Log.d(TAG, "onActivityResult: " + data.getStringExtra(Main2Activity.MY_KEY_RESULT));
     }
 }
